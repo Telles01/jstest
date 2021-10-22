fcdd2a8f516644fcf90073b4a50505fa7953f0d3
Bernard Telles <bernardtelgon@gmail.com>
2021-10-21 23:39:13 -0300

    test

--- dev1.js
+++ dev1.js
@@ -1,5 +1,6 @@
-let idade = 5;
-console.log(idade);
-let altura = 180;
-console.log(altura);
+//let idade = 5;
+//console.log(idade);
+//let altura = 180;
+//console.log(altura);
+let nome, idade, altura;
 

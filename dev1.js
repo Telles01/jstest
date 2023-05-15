9c088a437cce7f2e496d3c28cec766a0d3162d48
Bernard Telles <bernardtelgon@gmail.com>
2023-05-15 20:56:20 -0300

    teste

--- dev1.js
+++ dev1.js
@@ -11,11 +11,13 @@
 let nome = 'Carlos'; //string liberal
 let idade = 25; //number liberal
 let estaAprovado = true; //boolean
-let sobrenome = undefined; //undefined
+let sobrenome = undefined; //undefined / tHINK ABOUT THAT CODE 
 let corSelecionado = null; //
 
 
 let pessoa = {
+
+
   nome: 'Rafael',
   idade: 25,
   estaAprovado: true,
@@ -35,11 +37,41 @@ console.log(nameRoomMate[0]);
 let endereco = ["n1", "n2", "n3"];
 console.log(endereco[2]);
 
+
 let cpf = [0,1,2,3,4,5];
 console.log(cpf[0]);
 
 let parents = ['father', 'mother', 'sister', 'brother'];
 console.let(parents[1]);
 
+console.log(pessoa);
 
+let familia = [25,30,57,'Queiroz'];
+console-log(familia[2]);
+
+let nameRoomMate = ['Carlos', 'Jose','Maria','Zuzu'];
+console.log(nameRoomMate[0]);
+
+let endereco = ["n1", "n2", "n3"];
+console.log(endereco[2]);
+
+let cpf = [0,1,2,3,4,5];
+console.log(cpf[0]);
+
+let parents = ['father', 'mother', 'sister', 'brother'];
+console.log(parents[1]);
+
+let room = ['living room', 'bed room', 'kitchen', 'bathroom'];
+
+
+console.log(room[0]);
+
+let market = ['1', '2', '3', '4', '5','6'];
+console.log(market[0]);
+
+let suprem = ['Iphone 2g', 'Iphone 3', 'Iphone 3gs','Iphone 4', 'Iphone 4s', 'Iphone 5', 'Iphone 5s' ];
+console.log(smartphone[0]);
+
+let furniture = ['cama','cadeira','mesa','poltrona'];
+console.log(furniture[0]);
 
